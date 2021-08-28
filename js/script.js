0c0dea2ba63660b3eb91175420d233a0bd0ef999
NikitaOstrovsky636 <misha.ostrovsky1234567@gmail.com>
2021-08-28 22:42:59 +0300

    Add basic operations in calculator

--- js/script.js
+++ js/script.js
@@ -1,29 +1,65 @@
-function calcSum() { 
-    const a = +document.getElementById('a').value; 
-    const b = +document.getElementById('b').value;
-    const c1 = a + b;
-    document.getElementById('result').innerHTML = 'result ' + c1;   
- } 
- 
- function calcSub() {
-    const a = +document.getElementById('a').value; 
-    const b = +document.getElementById('b').value;
-    const c2 = a - b;
-    document.getElementById('result').innerHTML = 'result ' + c2;   
- }
+let globalNumbers = document.querySelectorAll('.action-bar__numbers'),
+    operators = document.querySelectorAll('.action-bar__operators'),
+    cleanEntry = document.getElementById('ce'),
+    output = document.getElementById('output'),
+    xNumber = null,
+    yNumber = '',
+    operator = '',
+    operatorClicked = false;
+    output.value = '0';
 
- function calcMul() {
-    const a = +document.getElementById('a').value; 
-    const b = +document.getElementById('b').value;
-    const c3 = a * b;
-    document.getElementById('result').innerHTML = 'result ' + c3;   
- }
+for (let i = 0; i < globalNumbers.length; i++) {
+    globalNumbers[i].addEventListener('click', event => 
+    numberOnClick(event.target.defaultValue)
+    );
+}
 
- function calcDiv() {
-    const a = +document.getElementById('a').value; 
-    const b = +document.getElementById('b').value;
-    const c4 = a / b;
-    document.getElementById('result').innerHTML = 'result ' + c4;   
- }
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', event =>
+    operation(event.target.defaultValue)
+    );
+}
 
- 
+cleanEntry.addEventListener('click', event =>
+clean(event.target.defaultValue)
+);
+
+function numberOnClick(num) { 
+    
+    if (operatorClicked === true) {
+        output.value = num; 
+        operatorClicked = false;
+    } else {
+        output.value === '0' ? output.value = num : output.value += num;
+    }
+}
+
+function operation(op) { 
+    if(xNumber === null) {
+    xNumber = output.value;
+    } else if (operator) {
+        const res = calculate(xNumber, output.value, operator);
+        output.value = res;
+        xNumber = res;
+    }
+    operator = op;
+    operatorClicked = true; 
+}
+
+function calculate(xNumber, yNumber, operator) {
+    xNumber = Number(xNumber);
+    yNumber = Number (yNumber);
+    switch (operator) {
+        case '+': return xNumber + yNumber;
+        case '-': return xNumber - yNumber;
+        case 'х': return xNumber * yNumber;
+        case '÷': return xNumber / yNumber;
+        case '=': return xNumber; 
+    }
+}
+
+function clean() {
+    output.value = '0';
+    xNumber = null;
+    yNumber = '';
+}
