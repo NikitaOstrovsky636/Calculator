036adedbde262632efea55eabbba2e0712269746
NikitaOstrovsky636 <misha.ostrovsky1234567@gmail.com>
2021-09-01 23:03:56 +0300

    Edd new operations in calculator and improved coding style in Java Script

--- js/script.js
+++ js/script.js
@@ -1,47 +1,38 @@
-let globalNumbers = document.querySelectorAll('.action-bar__numbers'),
-    operators = document.querySelectorAll('.action-bar__operators'),
-    cleanEntry = document.getElementById('ce'),
-    output = document.getElementById('output'),
-    xNumber = null,
-    yNumber = '',
-    operator = '',
-    operatorClicked = false;
-    output.value = '0';
+window.onload = initialize;
+let xNumber = null;
+let yNumber = '';
+let operator = '';
+let operatorClicked = false;
 
-for (let i = 0; i < globalNumbers.length; i++) {
-    globalNumbers[i].addEventListener('click', event => 
-    numberOnClick(event.target.defaultValue)
-    );
-}
-
-for (let i = 0; i < operators.length; i++) {
-    operators[i].addEventListener('click', event =>
-    operation(event.target.defaultValue)
-    );
-}
-
-cleanEntry.addEventListener('click', event =>
-clean(event.target.defaultValue)
-);
-
-function numberOnClick(num) { 
-    
-    if (operatorClicked === true) {
+function onNumberClick(num) { 
+    if (operatorClicked) {
         output.value = num; 
         operatorClicked = false;
     } else {
-        output.value === '0' ? output.value = num : output.value += num;
+       output.value === '0' ? output.value = num : output.value += num;
     }
 }
 
 function operation(op) { 
+    if(operator && operatorClicked){
+        operator = op;
+        return;
+    } else if (operator === '=') {
+        xNumber = output.value;
+    }
+
     if(xNumber === null) {
-    xNumber = output.value;
+        xNumber = output.value;
     } else if (operator) {
         const res = calculate(xNumber, output.value, operator);
         output.value = res;
         xNumber = res;
+    } 
+
+    if (xNumber === Infinity) {
+        output.value = 'You can not divide by 0';
     }
+
     operator = op;
     operatorClicked = true; 
 }
@@ -52,9 +43,9 @@ function calculate(xNumber, yNumber, operator) {
     switch (operator) {
         case '+': return xNumber + yNumber;
         case '-': return xNumber - yNumber;
-        case 'х': return xNumber * yNumber;
+        case 'x': return xNumber * yNumber;
         case '÷': return xNumber / yNumber;
-        case '=': return xNumber; 
+        case '=': return xNumber;
     }
 }
 
@@ -62,4 +53,66 @@ function clean() {
     output.value = '0';
     xNumber = null;
     yNumber = '';
+}
+
+function decimalEntry(dec) {
+    if (!output.value.includes(dec)) {
+        output.value += dec;
+    }
+    
+    if (operatorClicked) {
+        output.value = '0' + dec;
+        operatorClicked = false;
+    }
+}
+
+function numberNegative() {
+    output.value = -(output.value);
+}
+
+function rootSquare() {
+    if (output.value) {
+        output.value = Math.sqrt(output.value);
+        xNumber = output.value;
+        operatorClicked = true;
+    }
+}
+
+function initialize () {
+    const globalNumbers = document.querySelectorAll('.action-bar__number');
+    for (let i = 0; i < globalNumbers.length; i++) {
+        globalNumbers[i].addEventListener('click', event => 
+        onNumberClick(event.target.defaultValue)
+        );
+    }
+
+    const operators = document.querySelectorAll('.action-bar__operator');
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', event =>
+        operation(event.target.defaultValue)
+        );
+    }
+
+    const decimal = document.getElementById('dec');
+    decimal.addEventListener('click', event => 
+    decimalEntry(event.target.defaultValue)
+    );
+
+    const sqrt = document.getElementById('sqrt');
+    sqrt.addEventListener('click', () => 
+    rootSquare()
+    );
+
+    const cleanEntry = document.getElementById('ce');
+    cleanEntry.addEventListener('click', () =>
+    clean()
+    );
+
+    const negative = document.getElementById('negative');
+    negative.addEventListener('click', () => 
+    numberNegative()
+    );
+
+    const output = document.getElementById('output');
+    output.value = '0';
 }
