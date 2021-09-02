eed67e45f616f755086275c2f081c8b310d2481d
NikitaOstrovsky636 <misha.ostrovsky1234567@gmail.com>
2021-09-02 20:43:48 +0300

    Add simplified code and fix indentation prolems

--- js/script.js
+++ js/script.js
@@ -1,51 +1,57 @@
 window.onload = initialize;
+
 let xNumber = null;
 let yNumber = '';
 let operator = '';
 let operatorClicked = false;
 
-function onNumberClick(num) { 
+function onNumberClick(num) {
     if (operatorClicked) {
-        output.value = num; 
+        output.value = num;
         operatorClicked = false;
     } else {
-       output.value === '0' ? output.value = num : output.value += num;
+        output.value === '0' ? output.value = num : output.value += num;
     }
 }
 
-function operation(op) { 
-    if(operator && operatorClicked){
+function operation(op) {
+    if (operator && operatorClicked) {
         operator = op;
         return;
     } else if (operator === '=') {
         xNumber = output.value;
     }
 
-    if(xNumber === null) {
+    if (xNumber === null) {
         xNumber = output.value;
     } else if (operator) {
         const res = calculate(xNumber, output.value, operator);
         output.value = res;
         xNumber = res;
-    } 
+    }
 
     if (xNumber === Infinity) {
         output.value = 'You can not divide by 0';
     }
 
     operator = op;
-    operatorClicked = true; 
+    operatorClicked = true;
 }
 
 function calculate(xNumber, yNumber, operator) {
     xNumber = Number(xNumber);
-    yNumber = Number (yNumber);
+    yNumber = Number(yNumber);
     switch (operator) {
-        case '+': return xNumber + yNumber;
-        case '-': return xNumber - yNumber;
-        case 'x': return xNumber * yNumber;
-        case '÷': return xNumber / yNumber;
-        case '=': return xNumber;
+        case '+':
+            return xNumber + yNumber;
+        case '-':
+            return xNumber - yNumber;
+        case 'x':
+            return xNumber * yNumber;
+        case '÷':
+            return xNumber / yNumber;
+        case '=':
+            return xNumber;
     }
 }
 
@@ -59,7 +65,7 @@ function decimalEntry(dec) {
     if (!output.value.includes(dec)) {
         output.value += dec;
     }
-    
+
     if (operatorClicked) {
         output.value = '0' + dec;
         operatorClicked = false;
@@ -78,40 +84,34 @@ function rootSquare() {
     }
 }
 
-function initialize () {
+function initialize() {
     const globalNumbers = document.querySelectorAll('.action-bar__number');
     for (let i = 0; i < globalNumbers.length; i++) {
-        globalNumbers[i].addEventListener('click', event => 
-        onNumberClick(event.target.defaultValue)
+        globalNumbers[i].addEventListener('click', event =>
+            onNumberClick(event.target.defaultValue)
         );
     }
 
     const operators = document.querySelectorAll('.action-bar__operator');
     for (let i = 0; i < operators.length; i++) {
         operators[i].addEventListener('click', event =>
-        operation(event.target.defaultValue)
+            operation(event.target.defaultValue)
         );
     }
 
     const decimal = document.getElementById('dec');
-    decimal.addEventListener('click', event => 
-    decimalEntry(event.target.defaultValue)
+    decimal.addEventListener('click', event =>
+        decimalEntry(event.target.defaultValue)
     );
 
     const sqrt = document.getElementById('sqrt');
-    sqrt.addEventListener('click', () => 
-    rootSquare()
-    );
+    sqrt.onclick = rootSquare;
 
     const cleanEntry = document.getElementById('ce');
-    cleanEntry.addEventListener('click', () =>
-    clean()
-    );
+    cleanEntry.onclick = clean;
 
     const negative = document.getElementById('negative');
-    negative.addEventListener('click', () => 
-    numberNegative()
-    );
+    negative.onclick = numberNegative;
 
     const output = document.getElementById('output');
     output.value = '0';
